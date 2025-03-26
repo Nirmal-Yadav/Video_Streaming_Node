e9f66f7b3142de0f538e21547658068b1fc60e88
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-03-26 18:22:35 +0530

    utils method for Error Response Async Handler

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
-import express from "express";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-import run from "./db/index.js";
-
-const app = express();
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("serever running on port ", process.env.PORT);
+    });
+  })
+  .catch(() => {
+    console.log("momgo db connection failed");
+  });
+
 // run().catch(console.dir);
